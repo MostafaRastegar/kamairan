6a0907ae750921e0dae6f514121eb50e101b2013
hosseiny136 <hosseiny136@gmail.com>
2020-05-18 13:09:08 +0430

    profiletheme

--- app/components/ProfileTemplate/index.js
+++ app/components/ProfileTemplate/index.js
@@ -3,16 +3,19 @@ import './style.css';
 import { Button, Row, Col } from 'reactstrap';
 import SidebarMenu from '../SidebarMenu';
 const Profilethem = props => {
-  const { title, children, txt, notif } = props;
+  const { title, children, txt } = props;
   return (
     <Row>
       <Col xl="3">
-        <div className="ProfileSidebar"> <SidebarMenu notif="1"/></div></Col>
+        <div className="ProfileSidebar">
+          <SidebarMenu />
+        </div>
+      </Col>
       <Col xl="9">
         <div className="profile-theme">
           <div className="theme-top flex">
             <h1>{title}</h1>
-            { !!txt &&
+            {!!txt && (
               <Button
                 className="profile-theme-button"
                 style={{ display: 'block' }}
